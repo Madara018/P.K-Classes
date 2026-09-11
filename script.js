4e93fc8ebec225018c27e7678ebcf78416631af1
Madara018 <l67715053@gmail.com>
2026-09-12 00:55:30 +0530

    Implement UPI payment function in script.js
    Added payNow function for UPI payment processing.

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-﻿
-<script>
 function showMessage() {
     alert("Hello! Now we going for PDF!");
 }
-</script>
-  const upiId = "YOUR-UPI-ID"; 
+
+function payNow() {
+    const upiId = "YOUR-UPI-ID";
+
+    const paymentUrl =
+        "upi://pay?pa=" + upiId +
+        "&pn=PK%20Classes" +
+        "&am=10" +
+        "&cu=INR";
+
+    window.location.href = paymentUrl;
+}
