a0033d6b1983c77175f28861d707a63fced3d23b
Madara018 <l67715053@gmail.com>
2026-09-12 12:45:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function showMessage() {
 }
 
 function payNow() {
-    const upiId = "yourupi@bank";
+    const upiId = "YOUR-UPI-ID";
+
+    const amount = "10";
+    const note = "P.K Classes - 22 Days Subscription";
 
     const paymentUrl =
-        "upi://pay?pa=" + upiId +
-        "&pn=PK%20Classes" +
-        "&am=10" +
-        "&cu=INR";
+        `upi://pay?pa=${upiId}&pn=PK%20Classes&am=${amount}&cu=INR&tn=${encodeURIComponent(note)}`;
 
     window.location.href = paymentUrl;
 }
